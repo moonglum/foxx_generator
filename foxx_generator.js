6404f9f5a59443ef875c7b0f2ba72bd35ee06d14
Lucas Dohmen <lucas@dohmen.io>
2014-06-23 11:42:45 +0200

    JSLint

--- foxx_generator.js
+++ foxx_generator.js
@@ -4,17 +4,16 @@
 (function () {
   "use strict";
   var Foxx = require("org/arangodb/foxx"),
-    db = require("org/arangodb").db,
     _ = require("underscore"),
     Repository = {},
     State = {};
 
-  Repository.generate = function(options) {
+  Repository.generate = function (options) {
     var controller = new Foxx.Controller(options.applicationContext),
       model = options.contains,
       name = options.name,
       repository = new Foxx.Repository(controller.collection(name), { model: model }),
-      per_page = options.per_page,
+      per_page = options.per_page || 10,
       BodyParam,
       attributes = model.attributes;
 
@@ -22,23 +21,25 @@
 
     controller.get('/', function (req, res) {
       var data = {};
-      data[name] = _.map(repository.all({skip: 0, limit: 10}), function (datum) {
+      data[name] = _.map(repository.all({skip: 0, limit: per_page}), function (datum) {
         return datum.forClient();
       });
       res.json(data);
     }).summary('Get all entries').notes('Some fancy documentation');
 
-    controller.post('/', function(req, res) {
+    controller.post('/', function (req, res) {
       var data = {};
-      data[name] = _.map(req.params(name), function(model) {
+      data[name] = _.map(req.params(name), function (model) {
         return repository.save(model).forClient();
       });
       res.status(201);
       res.json(data);
-    }).bodyParam(name, 'TODO', [BodyParam]).summary('Post new entries').notes('Some fancy documentation');
+    }).bodyParam(name, 'TODO', [BodyParam])
+      .summary('Post new entries')
+      .notes('Some fancy documentation');
   };
 
-  State.generate = function(options) {
+  State.generate = function (options) {
     var attributes = options.attributes;
     return Foxx.Model.extend({}, { attributes: attributes });
   };
