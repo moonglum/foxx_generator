14f7509a9b865cdfd0f8361ec183eca921277928
Lucas Dohmen <lucas@dohmen.io>
2014-07-11 13:02:35 +0200

    Remove the scripts

--- scripts/setup.js
+++
@@ -1,7 +0,0 @@
-/*jslint indent: 2, nomen: true, maxlen: 100, white: true, plusplus: true, unparam: true */
-/*global todos*/
-/*global require, applicationContext*/
-
-(function() {
-  "use strict";
-}());
