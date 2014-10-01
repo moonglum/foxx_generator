f40455ff4fc9a18e2522fc6547f860763e579712
Lucas Dohmen <lucas@dohmen.io>
2014-10-01 22:14:45 +0200

    Extract Relation Repository

--- foxx_generator/json_api.js
+++ foxx_generator/json_api.js
@@ -16,7 +16,7 @@
     JsonApiState,
     ElementTransition,
     ContainerTransition,
-    RelationRepository,
+    RelationRepository = require('./relation_repository').RelationRepository,
     ReplaceOperation,
     transitions = [],
     generateLinkTemplates;
@@ -86,47 +86,6 @@
     op: { type: 'string', required: true },
     path: { type: 'string', required: true },
     value: { type: 'string', required: true }
-  });
-
-  RelationRepository = function (from, to, relation, graph) {
-    this.edgeCollectionName = relation.edgeCollectionName;
-    this.fromId = function (key) { return from.collectionName + '/' + key; };
-    this.toId = function (key) { return to.collectionName + '/' + key; };
-    this.graph = graph;
-  };
-
-  _.extend(RelationRepository.prototype, {
-    replaceRelation: function (sourceKey, destinationKey) {
-      var sourceId = this.fromId(sourceKey),
-        destinationId = this.toId(destinationKey),
-        edgeCollectionName = this.edgeCollectionName,
-        graph = this.graph;
-
-      graph.checkIfVerticesExist([destinationId, sourceId]);
-      graph.removeEdges({ vertexId: sourceId, edgeCollectionName: edgeCollectionName, throwError: false });
-      graph.createEdge({ edgeCollectionName: edgeCollectionName, sourceId: sourceId, destinationId: destinationId });
-    },
-
-    addRelations: function (sourceKey, destinationKeys) {
-      var sourceId = this.fromId(sourceKey),
-        destinationIds = _.map(destinationKeys, this.toId, this),
-        edgeCollectionName = this.edgeCollectionName,
-        graph = this.graph;
-
-      graph.checkIfVerticesExist(_.union(sourceId, destinationIds));
-      _.each(destinationIds, function (destinationId) {
-        graph.createEdge({ edgeCollectionName: edgeCollectionName, sourceId: sourceId, destinationId: destinationId });
-      });
-    },
-
-    deleteRelation: function (sourceKey) {
-      var sourceId = this.fromId(sourceKey),
-        edgeCollectionName = this.edgeCollectionName,
-        graph = this.graph;
-
-      graph.checkIfVerticesExist([sourceId]);
-      graph.removeEdges({ vertexId: sourceId, edgeCollectionName: edgeCollectionName });
-    }
   });
 
   Transition = BaseTransition.extend({
