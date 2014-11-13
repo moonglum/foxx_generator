dda46e54fc43b842ec8fb7d15f41c4ff0e84ef84
Lucas Dohmen <lucas@dohmen.io>
2014-11-13 15:12:21 +0100

    Inline variables to prepare refactoring

--- foxx_generator/siren.js
+++ foxx_generator/siren.js
@@ -24,13 +24,9 @@
     relation: 'one-to-one',
 
     executeOneToOne: function (controller, graph, relation, entityState, serviceState) {
-      var url = serviceState.urlTemplate,
-        verb = serviceState.verb,
-        repository = entityState.repository;
-
-      controller[verb](url, function (req, res) {
+      controller[serviceState.verb](serviceState.urlTemplate, function (req, res) {
         var id = req.params('id'),
-          entity = repository.byId(id);
+          entity = entityState.repository.byId(id);
 
         var opts = {
           superstate: {
@@ -53,24 +49,21 @@
     relation: 'one-to-one',
 
     executeOneToOne: function (controller, graph, relation, entityState) {
-      var url = entityState.urlTemplate,
-        nameOfRootElement = entityState.name,
-        BodyParam = Foxx.Model.extend({ schema: relation.parameters }),
-        repository = entityState.repository;
+      var BodyParam = Foxx.Model.extend({ schema: relation.parameters });
 
-      controller.patch(url, function (req, res) {
+      controller.patch(entityState.urlTemplate, function (req, res) {
         var id = req.params('id'),
-          patch = req.params(nameOfRootElement),
+          patch = req.params(entityState.name),
           result;
 
-        repository.updateById(id, patch.forDB());
-        result = repository.byIdWithNeighbors(id);
+        entityState.repository.updateById(id, patch.forDB());
+        result = entityState.repository.byIdWithNeighbors(id);
 
         res.json(result.forClient());
       }).errorResponse(ConditionNotFulfilled, 403, 'The condition could not be fulfilled')
         .onlyIf(relation.condition)
         .pathParam('id', joi.string().description('ID of the entity'))
-        .bodyParam(nameOfRootElement, 'TODO', BodyParam)
+        .bodyParam(entityState.name, 'TODO', BodyParam)
         .summary(relation.summary)
         .notes(relation.notes);
     }
@@ -83,10 +76,9 @@
     relation: 'one-to-one',
 
     executeOneToOne: function (controller, graph, relation, from, to) {
-      var url = from.urlForRelation(relation),
-        relationRepository = new RelationRepository(from, to, relation, graph);
+      var relationRepository = new RelationRepository(from, to, relation, graph);
 
-      controller.post(url, function (req, res) {
+      controller.post(from.urlForRelation(relation), function (req, res) {
         relationRepository.replaceRelation(req.params('id'), req.body()[relation.name]);
         res.status(204);
       }).pathParam('id', joi.string().description('ID of the document'))
@@ -98,10 +90,9 @@
     },
 
     executeOneToMany: function (controller, graph, relation, from, to) {
-      var url = from.urlForRelation(relation),
-        relationRepository = new RelationRepository(from, to, relation, graph);
+      var relationRepository = new RelationRepository(from, to, relation, graph);
 
-      controller.post(url, function (req, res) {
+      controller.post(from.urlForRelation(relation), function (req, res) {
         relationRepository.addRelations(req.params('id'), req.body()[relation.name]);
         res.status(204);
       }).pathParam('id', joi.string().description('ID of the document'))
@@ -120,10 +111,9 @@
     relation: 'one-to-one',
 
     executeOneToOne: function (controller, graph, relation, from, to) {
-      var url = from.urlForRelation(relation),
-        relationRepository = new RelationRepository(from, to, relation, graph);
+      var relationRepository = new RelationRepository(from, to, relation, graph);
 
-      controller.delete(url, function (req, res) {
+      controller.delete(from.urlForRelation(relation), function (req, res) {
         relationRepository.deleteRelation(req.params('id'));
         res.status(204);
       }).pathParam('id', joi.string().description('ID of the document'))
@@ -151,8 +141,7 @@
     to: 'entity',
 
     executeOneToOne: function (controller, graph, relation, repositoryState, entityState) {
-      var nameOfRootElement = entityState.name,
-        repository = repositoryState.repository,
+      var repository = repositoryState.repository,
         BodyParam = Foxx.Model.extend({ schema: relation.parameters }),
         url = repositoryState.urlTemplate,
         name = relation.name,
@@ -179,12 +168,12 @@
 
       controller.post(url, function (req, res) {
         var data = {},
-          model = req.params(nameOfRootElement);
+          model = req.params(entityState.name);
 
-        data[nameOfRootElement] = repository.save(model).forClient();
+        data[entityState.name] = repository.save(model).forClient();
         res.status(201);
         res.json(data);
-      }).bodyParam(nameOfRootElement, 'TODO', BodyParam)
+      }).bodyParam(entityState.name, 'TODO', BodyParam)
         .errorResponse(ConditionNotFulfilled, 403, 'The condition could not be fulfilled')
         .onlyIf(relation.condition)
         .summary(relation.summary)
@@ -203,14 +192,9 @@
     },
 
     executeOneToOne: function (controller, graph, relation, repositoryState, entityState) {
-      var rel = relation.name,
-        url = entityState.urlTemplate,
-        title = relation.summary,
-        repository = repositoryState.repository;
-
-      controller.get(url, function (req, res) {
+      controller.get(entityState.urlTemplate, function (req, res) {
         var id = req.params('id'),
-          entry = repository.byIdWithNeighbors(id);
+          entry = repositoryState.repository.byIdWithNeighbors(id);
 
         res.json(entry.forClient());
       }).pathParam('id', joi.string().description('ID of the document'))
@@ -219,7 +203,7 @@
         .summary(relation.summary)
         .notes(relation.notes);
 
-      repositoryState.addLinkToEntities(rel, url, title, entityState);
+      repositoryState.addLinkToEntities(relation.name, entityState.urlTemplate, relation.summary, entityState);
     }
   });
 
@@ -229,14 +213,9 @@
     to: 'repository',
 
     executeOneToOne: function (controller, graph, relation, from, to) {
-      var rel = relation.name,
-        url = to.urlTemplate,
-        precondition = relation.precondition,
-        title = relation.summary;
+      from.addLink([relation.name], to.urlTemplate, relation.summary, relation.precondition);
 
-      from.addLink([rel], url, title, precondition);
-
-      controller.get(url, function (req, res) {
+      controller.get(to.urlTemplate, function (req, res) {
         res.json({
           properties: to.properties(),
           entities: to.entities(),
@@ -256,19 +235,12 @@
     to: 'service',
 
     executeOneToOne: function (controller, graph, relation, from, to) {
-      var rel = relation.name,
-        url = to.urlTemplate,
-        title = relation.summary,
-        verb = to.verb,
-        action = to.action,
-        nameOfRootElement = to.name,
-        precondition = relation.precondition,
-        BodyParam = Foxx.Model.extend({ schema: relation.parameters });
+      var BodyParam = Foxx.Model.extend({ schema: relation.parameters });
 
-      from.addLink([rel], url, title, precondition);
+      from.addLink([relation.name], to.urlTemplate, relation.summary, relation.precondition);
 
-      controller[verb](url, action)
-        .bodyParam(nameOfRootElement, 'TODO', BodyParam)
+      controller[to.verb](to.urlTemplate, to.action)
+        .bodyParam(to.name, 'TODO', BodyParam)
         .errorResponse(ConditionNotFulfilled, 403, 'The condition could not be fulfilled')
         .onlyIf(relation.condition)
         .summary(relation.summary)
