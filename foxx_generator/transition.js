a9ea2ece6526d2f887fa9e8f7b4cc098587f5696
Lucas Dohmen <lucas@dohmen.io>
2014-11-14 13:52:32 +0100

    Further simplifications

--- foxx_generator/transition.js
+++ foxx_generator/transition.js
@@ -3,7 +3,16 @@
   var extend = require('org/arangodb/extend').extend,
     ConditionNotFulfilled = require('./condition_not_fulfilled').ConditionNotFulfilled,
     _ = require('underscore'),
+    wrapCondition,
     Transition;
+
+  wrapCondition = function (condition) {
+    return function (req) {
+      if (!condition(req)) {
+        throw new ConditionNotFulfilled('Condition was not fulfilled');
+      }
+    };
+  };
 
   Transition = function (graph, controller) {
     this.graph = graph;
@@ -13,23 +22,8 @@
   _.extend(Transition.prototype, {
     edgeCollectionName: function (from, to) { return this.collectionBaseName + '_' + from.name + '_' + to.name; },
 
-    addRoutes: function (controller, graph, relation, from, to, cardinality) {
-      var context = new this.Context(this.type, from.type, to.type, cardinality);
-      context.execute(controller, graph, relation, from, to);
-    },
-
-    apply: function (from, to) {
-      var condition, conditionWrapper, relation;
-
-      condition = this.condition;
-
-      conditionWrapper = function (req) {
-        if (!condition(req)) {
-          throw new ConditionNotFulfilled('Condition was not fulfilled');
-        }
-      };
-
-      relation = {
+    relationBetween: function (from, to) {
+      return {
         name: this.relationName,
         edgeCollectionName: this.graph.extendEdgeDefinitions(this.edgeCollectionName(from, to), from, to),
         cardinality: this.cardinality,
@@ -37,14 +31,19 @@
         parameters: this.parameters,
         summary: this.summary,
         notes: this.notes,
-        condition: conditionWrapper,
+        condition: wrapCondition(this.condition),
         precondition: this.precondition,
         to: to
       };
+    },
+
+    apply: function (from, to) {
+      var relation = this.relationBetween(from, to),
+        context = new this.Context(this.type, from.type, to.type, 'one-to-' + relation.cardinality);
 
       from.relations.push(relation);
 
-      this.addRoutes(this.controller, this.graph, relation, from, to, 'one-to-' + relation.cardinality);
+      context.execute(this.controller, this.graph, relation, from, to);
     }
   });
 
